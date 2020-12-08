3d7c11af2971f0c857879d451d31a9e28ba5a9c7
1278066918@qq.com <1278066918@qq.com>
2020-12-08 21:22:18 +0800

    adb -s 基本命令修改

--- adb.py
+++ adb.py
@@ -2,14 +2,15 @@ import os
 import time
 import subprocess
 
+
 # 根据坐标点击228 174  1000 28
-def AdbShellInputTap(x, y):
-    os.system("adb shell input tap %s %s" % (x, y))
+def AdbShellInputTap(dev, x, y):
+    os.system("adb -s %s shell input tap %s %s" % (dev, x, y))
 
 
 # 输入字符
-def AdbShellInputText(text):
-    os.system("adb shell input text %s" % text)
+def AdbShellInputText(dev, text):
+    os.system("adb -s %s shell input text %s" % (dev, text))
 
 
 # 等待时间
@@ -18,22 +19,24 @@ def TimeSleepDuration(x):
 
 
 # 保存截图到指定路径
-def AdbShellScreencapPullRm(path):
-    os.system("adb shell screencap -p /sdcard/screen.png")
-    os.system("adb pull /sdcard/screen.png %s" % path)
+def AdbShellScreencapPullRm(dev, path):
+    os.system("adb -s %s shell screencap -p /sdcard/screen.png" % dev)
+    os.system("adb -s %s pull /sdcard/screen.png %s" % (dev, path))
 
 
 # //查看手机上第三方应用的packageName
 # //adb shell pm list packages -3
-def AdbShellPmListPackages():
-    data = subprocess.Popen("adb shell pm list packages -3",shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+def AdbShellPmListPackages(dev):
+    data = subprocess.Popen("adb -s %s shell pm list packages -3" % dev, shell=True, stdout=subprocess.PIPE,
+                            encoding='utf-8')
     return data.stdout.read()
 
+
 # 测试点击
-# AdbShellInputTap(228, 174)
+# AdbShellInputTap("127.0.0.1:7555", 228, 174)
 # time.sleep(1)
-# AdbShellInputTap(1000, 28)
+# AdbShellInputTap("127.0.0.1:7555", 1000, 28)
 # time.sleep(1)
-# AdbShellInputText("ssss")
+# AdbShellInputText("127.0.0.1:7555", "ssss")
 # AdbShellScreencapPullRm(r"D:\bao\1\0531落地页")
-# print(AdbShellPmListPackages())
+# print(AdbShellPmListPackages("127.0.0.1:7555"))
