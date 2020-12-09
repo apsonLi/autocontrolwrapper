518fcb068f7ff6156b2227e1043a0b4d8e18759d
1278066918@qq.com <1278066918@qq.com>
2020-12-09 12:52:40 +0800

    OCR编写(引入类)

--- adb.py
+++ adb.py
@@ -3,40 +3,39 @@ import time
 import subprocess
 
 
-# 根据坐标点击228 174  1000 28
-def AdbShellInputTap(dev, x, y):
-    os.system("adb -s %s shell input tap %s %s" % (dev, x, y))
+class AdbBase:
+    def __init__(self, dev):
+        self.dev = dev
 
+    # 根据坐标点击228 174  1000 28
+    def AdbShellInputTap(self, x, y):
+        os.system("adb -s %s shell input tap %s %s" % (self.dev, x, y))
 
-# 输入字符
-def AdbShellInputText(dev, text):
-    os.system("adb -s %s shell input text %s" % (dev, text))
+    # 输入字符
+    def AdbShellInputText(self, text):
+        os.system("adb -s %s shell input text %s" % (self.dev, text))
 
+    # 等待时间
+    def TimeSleepDuration(self, x):
+        time.sleep(x)
 
-# 等待时间
-def TimeSleepDuration(x):
-    time.sleep(x)
+    # 保存截图到指定路径
+    def AdbShellScreencapPullRm(self, path):
+        os.system("adb -s %s shell screencap -p /sdcard/screen.png" % self.dev)
+        os.system("adb -s %s pull /sdcard/screen.png %s" % (self.dev, path))
 
-
-# 保存截图到指定路径
-def AdbShellScreencapPullRm(dev, path):
-    os.system("adb -s %s shell screencap -p /sdcard/screen.png" % dev)
-    os.system("adb -s %s pull /sdcard/screen.png %s" % (dev, path))
-
-
-# //查看手机上第三方应用的packageName
-# //adb shell pm list packages -3
-def AdbShellPmListPackages(dev):
-    data = subprocess.Popen("adb -s %s shell pm list packages -3" % dev, shell=True, stdout=subprocess.PIPE,
-                            encoding='utf-8')
-    return data.stdout.read()
-
+    # //查看手机上第三方应用的packageName
+    # //adb shell pm list packages -3
+    def AdbShellPmListPackages(self):
+        data = subprocess.Popen("adb -s %s shell pm list packages -3" % self.dev, shell=True, stdout=subprocess.PIPE,
+                                encoding='utf-8')
+        return data.stdout.read()
 
 # 测试点击
-# AdbShellInputTap("127.0.0.1:7555", 228, 174)
+# Adb_base("127.0.0.1:7555").AdbShellInputTap(228, 174)
 # time.sleep(1)
-# AdbShellInputTap("127.0.0.1:7555", 1000, 28)
+# Adb_base("127.0.0.1:7555").AdbShellInputTap(1000, 28)
 # time.sleep(1)
-# AdbShellInputText("127.0.0.1:7555", "ssss")
-# AdbShellScreencapPullRm(r"D:\bao\1\0531落地页")
-# print(AdbShellPmListPackages("127.0.0.1:7555"))
+# Adb_base("127.0.0.1:7555").AdbShellInputText("ssss")
+# Adb_base("127.0.0.1:7555").AdbShellScreencapPullRm(r"D:\bao\1\0531落地页")
+# print(Adb_base("127.0.0.1:7555").AdbShellPmListPackages())
--- ocr.py
+++ ocr.py
@@ -1,21 +1,54 @@
 from aip import AipOcr
 
-APP_ID = '17975398'
-API_KEY = 'n8bGMLwndukfywu18yKLuNYI'
-SECRET_KEY = 'fNqBSM0A9gRloci2kv139ZlxL7aGSfor'
-client = AipOcr(APP_ID, API_KEY, SECRET_KEY)
+_APP_ID = '17975398'
+_API_KEY = 'n8bGMLwndukfywu18yKLuNYI'
+_SECRET_KEY = 'fNqBSM0A9gRloci2kv139ZlxL7aGSfor'
+_client = AipOcr(_APP_ID, _API_KEY, _SECRET_KEY)
 
 
-def get_file_content(filePath):
-    with open(filePath, 'rb') as fp:
-        return fp.read()
+class OCRbaidu:
+    def __init__(self, filepath):
+        self.filepath = filepath
+
+    def _get_file_content(self):
+        with open(self.filepath, 'rb') as fp:
+            return fp.read()
+
+    # basicAccurate 高精准 basicGeneral 通用
+    # 高精准识别图片用于识别区服，不带坐标
+    def ocr_QFdata(self):
+        image = self._get_file_content()
+        resluts = _client.basicAccurate(image)
+        # print(resluts)
+        return resluts["words_result"][0]["words"]
+
+    # 返回坐标 普通版
+    def ocr_Tap(self, value):
+        tap = []
+        image = self._get_file_content()
+        result = _client.general(image)
+        data = result["words_result"]
+
+        for i in data:
+            if value in i['words']:
+                tap.append(i["location"]["left"])
+                tap.append(i["location"]["top"])
+                return tap
+
+    # 返回坐标 精准版
+    def ocr_Tap2(self, value):
+        tap = []
+        image = self._get_file_content()
+        result = _client.accurate(image)
+        data = result["words_result"]
+
+        for i in data:
+            if value in i['words']:
+                tap.append(i["location"]["left"])
+                tap.append(i["location"]["top"])
+                return tap
 
 
-# basicAccurate 高精准 basicGeneral 通用
-def ocr_data(path):
-    image = get_file_content(path)
-    resluts = client.basicAccurate(image)
-    # print(resluts)
-    return resluts["words_result"][0]["words"]
-
-# print(ocr_data(r"D:\PyCharm\space\testApi\picture\hmy2.png"))
+# 测试
+# print(OCRbaidu(r"D:\PyCharm\space\testApi\picture\hmy2.png").ocr_QFdata())
+# print(OCRbaidu(r"D:\PyCharm\space\testApi\picture\hmy.png").ocr_Tap("进入游戏"))
