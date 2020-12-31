c22a578b891f9e3da1c6224d85f76c26b697c59b
1278066918@qq.com <1278066918@qq.com>
2020-12-31 10:16:56 +0800

    修改adb，项目可正常使用

--- adb.py
+++ adb.py
@@ -3,29 +3,44 @@ import time
 import subprocess
 import xmltodict
 import json
-import re
+from ocr import OCRbase
+import argparse
 
 
 class AdbBase:
-    def __init__(self, dev):
+    def __init__(self, dev, p=None, w=None):
         self.dev = dev
+        self.p = p
+        self.w = w
 
     # 根据坐标点击228 174  1000 28
     def AdbShellInputTap(self, x, y):
-        os.system("adb -s %s shell input tap %s %s" % (self.dev, x, y))
+        os.popen("adb -s %s shell input tap %s %s" % (self.dev, x, y))
 
     # 输入字符
     def AdbShellInputText(self, text):
-        os.system("adb -s %s shell input text %s" % (self.dev, text))
+        os.popen("adb -s %s shell input text %s" % (self.dev, text))
 
     # 等待时间
     def TimeSleepDuration(self, x):
         time.sleep(x)
 
     # 保存截图到指定路径
-    def AdbShellScreencapPullRm(self, path):
-        os.system("adb -s %s shell screencap -p /sdcard/screen.png" % self.dev)
-        os.system("adb -s %s pull /sdcard/screen.png %s" % (self.dev, path))
+    def AdbShellScreencapPullRm(self, path, name):
+        if self.p is None:
+            subprocess.Popen("adb -s %s shell screencap -p /sdcard/%s.png > sdcard/info.txt" % (
+                self.dev, name))
+            time.sleep(2)
+            os.popen("adb -s %s pull /sdcard/%s.png %s >log.txt" % (self.dev, name, path))
+            time.sleep(2)
+
+            # 保存截图到指定路径
+
+    def AdbShellScreencapPullRm2(self, path):
+        if self.p is not None:
+            subprocess.Popen("adb -s %s shell screencap -p /sdcard/%s.png" % (self.dev, self.p))
+            time.sleep(1)
+            subprocess.Popen("adb -s %s pull /sdcard/%s.png %s" % (self.dev, self.p, path))
 
     # //查看手机上第三方应用的packageName
     # //adb shell pm list packages -3
@@ -34,32 +49,39 @@ class AdbBase:
                                 encoding='utf-8')
         return data.stdout.read()
 
-    def __adbDump(self):
+    def __adbDump(self, apkName):
         path = os.getcwd()
-        os.system("adb -s %s shell uiautomator dump --compressed /sdcard/window_dump.xml" % self.dev)
-        os.system("adb -s %s pull /sdcard/window_dump.xml %s" % (self.dev, path))
-        return os.path.join(path, "window_dump.xml")
+        if self.w != None:
+            path = self.w
+        # os.path.abspath()
+        subprocess.Popen(
+            "adb -s %s shell uiautomator dump --compressed /sdcard/%s.xml > sdcard/info.txt" % (self.dev, apkName))
+        time.sleep(2)
+        os.popen("adb -s %s pull /sdcard/%s.xml %s >log.txt" % (self.dev, apkName, path))
+        return path
 
     # 根据字符，在dump文件中查找对应坐标 path:文件地址，不包括文件名，返回是：[[224.0, 174.5], [968.0, 468.5]]
-    def adbDumpTap(self, text):
-        file = self.__adbDump()
+    def adbDumpTap(self, text, path):
+        apkname, _ = self.getAPKInfo(path)
+        file = self.__adbDump(apkname)
         json_data = self.xml_to_json(file)
-        self.__cyclefromNode(json_data["hierarchy"],text)
+        self.__cyclefromNode(json_data["hierarchy"], text)
 
-    def __cyclefromNode(self,json,text):
-        #遍历每个Node节点
-        if isinstance(json,dict) and 'node' in json.keys():
-            self.__cyclefromNode(json['node'],text)
-        elif isinstance(json,list) or isinstance(json,tuple):
+    def __cyclefromNode(self, json, text):
+        # 遍历每个Node节点
+        if isinstance(json, dict) and 'node' in json.keys():
+            self.__cyclefromNode(json['node'], text)
+
+        elif isinstance(json, list) or isinstance(json, tuple):
             for item in json:
-                self.__cyclefromNode(item,text)
+                self.__cyclefromNode(item, text)
         else:
+            # print(json)
             if text == json["@text"]:
                 point = json["@bounds"].split('][')[0][1:].split(",")
-                self.AdbShellInputTap(point[0],point[1])
+                # print(point)
+                self.AdbShellInputTap(point[0], point[1])
                 return True
-            
-
 
     # xml文件转换成json
     def xml_to_json(self, xml):
@@ -72,10 +94,11 @@ class AdbBase:
 
     # 返回packageName 和 launcher_act
     def getAPKInfo(self, filePath):
-        data = os.popen("aapt dump badging %s | findstr package" % filePath).read()
-        apkname = data.split('\'')[1]
+        data = subprocess.Popen("aapt dump badging %s | findstr package" % filePath,
+                                shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+        apkname = data.stdout.read().split('\'')[1]
 
-        data3 = subprocess.Popen("aapt dump badging %s | findstr launchable-activity" % filePath,
+        data3 = subprocess.Popen("aapt dump badging %s | findstr launchable" % filePath,
                                  shell=True, stdout=subprocess.PIPE, encoding='utf-8')
         launchable_activity = data3.stdout.read().split('\'')[1]
 
@@ -84,7 +107,7 @@ class AdbBase:
     # 安装apk
     def install_apk(self, path):
         result = subprocess.Popen("adb -s %s install %s" % (self.dev, path),
-                                 shell=True, stdout=subprocess.PIPE, encoding='utf-8').stdout.read()
+                                  shell=True, stdout=subprocess.PIPE, encoding='utf-8').stdout.read()
         if 'Success' in result:
             return True
         else:
@@ -93,15 +116,20 @@ class AdbBase:
     # 卸载apk
     def uninstall_apk(self, path):
         apkname, _ = self.getAPKInfo(path)
-        os.system("adb -s %s uninstall %s" % (self.dev, apkname))
+        subprocess.Popen("adb -s %s uninstall %s" % (self.dev, apkname))
 
     # 启动apk
     def start_apk(self, path):
-        apkname, activity = self.getAPKInfo(path)
-        os.system("adb -s {0} shell am start -n {1}/{2}".format(self.dev, apkname, activity))
+        try:
+            apkname, activity = self.getAPKInfo(path)
+            subprocess.Popen(
+                "adb -s {0} shell am start -n {1}/{2} > sdcard/info.txt".format(self.dev, apkname, activity))
+        except:
+            err = "apk路径错误"
+            return err
 
-    #判断是否存在包名
-    def findIsExitPackage(self,path):
+    # 判断是否存在包名
+    def findIsExitPackage(self, path):
         apkname, _ = self.getAPKInfo(path)
         data = self.AdbShellPmListPackages()
         for i in data:
@@ -110,37 +138,72 @@ class AdbBase:
             else:
                 return False
 
-
-    #判断模拟器是否在运行中
+    # 判断模拟器是否在运行中
     def detectIsWorking(self):
-        result = subprocess.Popen("adb shell dumpsys activity | grep mResumedActivity" ,
-                                 shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+        result = subprocess.Popen("adb shell dumpsys activity | grep mResumedActivity",
+                                  shell=True, stdout=subprocess.PIPE, encoding='utf-8')
         result = result.stdout.read()
-        if result != None and 'com.vphone.launcher/.Launcher' in  result:
+        if result != None and 'com.vphone.launcher/.Launcher' in result:
             return False
         else:
             return True
 
     # 关闭当前应用
-    def killCurrentGame(self,apkname):
-        os.system("adb -s %s shell am force-stop %s" % (self.dev, apkname))
+    def killCurrentGame(self, path):
+        apkname, _ = self.getAPKInfo(path)
+        subprocess.Popen("adb -s %s shell am force-stop %s" % (self.dev, apkname))
 
 
+def allStart():
+    parser = argparse.ArgumentParser(description="powered from apson")
+    parser.add_argument('-s', '--sid', help='输入设备id')
+    parser.add_argument('-p', '--picturepath', help='图片地址')
+    parser.add_argument('-apkPath', '---apkPath', help='apk路径')
+    parser.add_argument('-w', '---windowpath', help='window.xml 路径')
+    args = parser.parse_args()
 
+    sid = args.sid
+    picture = args.picturepath
+    apkPath = args.apkPath
+    windowpath = args.windowpath
+
+    adb = AdbBase(sid, picture, windowpath)
+    ocr = OCRbase(picture)
+    return picture, apkPath, adb, ocr
+
+
+def dataRe(state=0, err="", datas=""):
+    if state == 1 or state is None:
+        value = {
+            "state": state,
+            "data": {
+                "districtName": "",
+            },
+            "err": err
+        }
+    else:
+        value = {
+            "state": state,
+            "data": {
+                "districtName": datas,
+            },
+            "err": ""
+        }
+    return value
 
 # 测试点击
-# Adb_base("127.0.0.1:7555").AdbShellInputTap(228, 174)
+# AdbBase("127.0.0.1:62001").AdbShellInputTap(1036, 225)
 # time.sleep(1)
 # Adb_base("127.0.0.1:7555").AdbShellInputTap(1000, 28)
 # time.sleep(1)
 # Adb_base("127.0.0.1:7555").AdbShellInputText("ssss")
-# Adb_base("127.0.0.1:7555").AdbShellScreencapPullRm(r"D:\bao\1\0531落地页")
+# AdbBase("127.0.0.1:62001").AdbShellScreencapPullRm(r"D:\bao\1\0531落地页")
 # print(Adb_base("127.0.0.1:7555").AdbShellPmListPackages())
 # print(AdbBase("127.0.0.1:7555").Adbdump(r"D:\bao\1\0531落地页"))
 # print(AdbBase("127.0.0.1:7555").xml_to_json(r"D:\bao\1\0531落地页\window_dump.xml"))
 # print(AdbBase("127.0.0.1:7555").Adbdump_Tap(r"D:\bao\1\0531落地页", "微信"))
-a= AdbBase("3EP0219114001784")
-print(a.adbDumpTap("知乎"))
+# a = AdbBase("127.0.0.1:62001")
+# a.adbDumpTap("游客登录")
 # a= AdbBase("127.0.0.1:62001").install_apk(r"D:\bao\apk\20201130\222.apk")
 
 # print(a)
--- ocr.py
+++ ocr.py
@@ -4,6 +4,7 @@ import time
 import hashlib
 import base64
 import json
+import os
 import cv2
 
 # baidu_ocr
@@ -20,10 +21,11 @@ API_KEY = "b9db4d674b5f3af780f2ab665ec67527"
 
 class OCRbase:
     # 默认不裁剪，如果有裁剪需求，isFix 为True 时是裁剪模式，需要传入坐标
-    def __init__(self, filepath, isFix=False, topX=0, bottomX=0, topY=0, bottomY=0):
+    def __init__(self, filepath, isFix=False, topX=0, bottomX=0, topY=0, bottomY=0, name="Screen"):
         self.filepath = filepath
+        self.name = name
         if isFix:
-            self.filepath = self.__opencv_Picture(topX, bottomX, topY, bottomY)
+            self.filepath = self.__opencv_Picture(topX, bottomX, topY, bottomY, self.name)
 
     def _get_file_content(self):
         with open(self.filepath, 'rb') as fp:
@@ -32,10 +34,14 @@ class OCRbase:
     # basicAccurate 高精准 basicGeneral 通用
     # 高精准识别图片用于识别区服，不带坐标
     def ocr_QFdata(self):
-        image = self._get_file_content()
-        resluts = _client.basicAccurate(image)
-        # print(resluts)
-        return resluts["words_result"][0]["words"]
+        try:
+            image = self._get_file_content()
+            resluts = _client.basicAccurate(image)
+            # print(resluts)
+            return resluts["words_result"][0]["words"]
+        except:
+            state = 1
+            return state
 
     # 返回坐标 普通版
     def ocr_Tap(self, value):
@@ -107,17 +113,29 @@ class OCRbase:
                     return k["content"]
 
     # opencv进行屏幕截取并重新生成新图片如：hmy.png->hmy2.png
-    def __opencv_Picture(self, x1, x2, y1, y2):
-        img = cv2.imread(self.filepath)
-        # cv2.namedWindow("Image")
-        img = img[y1:y2, x1:x2]
-        new_file = self.filepath.split(".")[0]
-        name_picture = new_file + "_refix" + ".png"
-        cv2.imwrite(name_picture, img)
-        return name_picture
+    def __opencv_Picture(self, x1, x2, y1, y2, name):
+        if "png" or "jpg" in self.filepath:
+            path=self.filepath
+        else:
+            path = os.path.join(self.filepath, "%s.png" % name)
+        try:
+            img = cv2.imread(path)
+            # cv2.namedWindow("Image")
+            img = img[y1:y2, x1:x2]
+            new_file = path.split(".")[0] + path.split(".")[1] + path.split(".")[2] + path.split(".")[3]
+            name_picture = new_file + "_refix" + ".png"
+            cv2.imwrite(name_picture, img)
+            return name_picture
+        except IndexError:
+            new_file = path.split(".")[0]
+            name_picture = new_file + "_refix" + ".png"
+            cv2.imwrite(name_picture, img)
+            return name_picture
 
 # 测试
 # print(OCRbaidu(r"D:\PyCharm\space\testApi\picture\hmy2.png").ocr_QFdata())
 # print(OCRbaidu(r"D:\PyCharm\space\testApi\picture\hmy.png").ocr_Tap2("进入游戏"))
 # print(OCRbase(r"D:\PyCharm\space\testApi\picture\hmy.png").ocr_QFdata_xf("进入游戏"))
 # print(OCRbase(r"/Users/apson/Desktop/30600.png").ocr_Tap('进入游戏')))
+# data = OCRbase(r"D:\test", isFix=True, topX=558, bottomX=983, topY=515, bottomY=567).ocr_QFdata()
+# print(data)
